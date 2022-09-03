dd878de05591c1ee1696b24ac6f7b1bc9f7bebb1
Igor Bubelov <igor@bubelov.com>
2022-09-03 12:49:40 +0700

    Do not commit trivial changes

--- refresh-data.sh
+++ refresh-data.sh
@@ -4,6 +4,19 @@ curl -X POST -d "@query.txt" -o new-data.json https://overpass-api.de/api/interp
 
 lines=$(wc -l new-data.json | cut --delimiter=' ' --fields 1)
 
-if [ $lines -gt 1000 ]; then
-    mv new-data.json data.json
+if [ "$lines" -lt 1000 ]; then
+    printf 'The response is too small (%s lines)\n' "$lines"
+    rm new-data.json
+    exit 0
+fi
+
+mv new-data.json data.json
+
+insertions=$(git diff --numstat data.json | cut --fields=1)
+deletions=$(git diff --numstat data.json | cut --fields=2)
+printf 'Insertions: %s, deletions: %s\n' "$insertions" "$deletions"
+
+if [ "$insertions" -eq 1 ] && [ "$deletions" -eq 1 ]; then
+    printf 'The difference is too small\n'
+    git checkout data.json
 fi
