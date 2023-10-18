b10118ba9db35f8a6fb06341ad548b10b4b7a6cb
dadofsambonzuki <nathan@day.ag>
2023-10-18 21:29:32 +0100

    Updated areas script to calculate areas based on the geojson.

--- utility-scripts/update-areas-with-km2.py
+++ utility-scripts/update-areas-with-km2.py
@@ -14,7 +14,7 @@ if not btcmap_api_token:
     sys.exit(1)
 
 # Define the API URL to fetch all areas
-url = "https://api.btcmap.org/v2/areas"
+url = "https://api.btcmap.org/areas"
 headers = {
     'Authorization': f'Bearer {btcmap_api_token}',
     'Content-Type': 'application/json'
@@ -31,44 +31,45 @@ if response.status_code != 200:
 areas = response.json()
 
 # Define the area type and area ID to filter (replace with your values)
-target_area_type = "community"
+area_type_filter = "community"
+area_alias_filter = "bansko-bitcoin"
 
 # Now, iterate through the features and treat each one as an area
 for id in areas:
-    area_id = id.get('id')
-    tags = id.get('tags', {})
+    tags = id.get('tags', None)
+    area_alias = tags.get('url_alias')
+    area_type = tags.get('type')
+    update_url = f"https://api.btcmap.org/areas/{area_alias}/tags"
 
-    print(area_id)
-    
-    if tags.get('type') == target_area_type:
-        geojson = tags.get('geo_json', {})
-        features = geojson["features"]
-        
-        # Now you can work with the features, which is a list of GeoJSON features
-        for feature in features:
-        
+    if area_type == area_type_filter:
+        geojson = tags.get('geo_json', None)
+
+        if geojson is not None:
+                      
             # Calculate the area of the geometry
-            area_m2 = area_calc(feature.get("geometry"))
+            area_m2 = area_calc(geojson)
             area_km2 = round(area_m2 / 1_000_000)
 
-            update_url = f"https://btcmap.org/areas/{area_id}/tags"
+            if area_km2 != 0:
+                
+                # Define the payload to update the 'km2' tag
+                area_km2_json = {
+                                    "area_km2": area_km2
+                                }
 
-        # Define the payload to update the 'km2' tag
-        update_payload = {
-            'tags': [
-                {
-                    'area_km2': area_km2
-                }
-            ]
-        }
-    
-        print(update_payload)
+                payload = json.dumps(area_km2_json)
+                
+                # Send a PATCH request to update the 'km2' tag
+                response = requests.patch(update_url, headers=headers, data=payload)
 
-        # Send a PATCH request to update the 'km2' tag
-        #response = requests.patch(update_url, headers=headers, json=update_payload)
-
-        #if response.status_code == 200:
-        #    print(f"Updated 'km2' for area ID {area_id} to {area_km2} km2")
+                if response.status_code == 200:
+                    print(f"Updated 'km2' for area ID {area_alias} to {area_km2} km2")
+                else:
+                    print(f"Error updating {area_alias}: {response.text}")
+            
+            else:
+                print(f"{area_alias} has an 0 KM2 value")
         
-        #else:
-        #    print(f"Error updating 'km2' for area ID {area_id}: {response.text}")
+        else:
+
+            print(f"{area_alias} does not have a geojson")
